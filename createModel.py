64aec16f8888113549e9540fd19b836472b593c1
Apostol6666 <44125687+Apostol6666@users.noreply.github.com>
2020-06-19 20:27:44 +0800

    Update createModel.py

--- createModel.py
+++ createModel.py
@@ -62,29 +62,21 @@ y_train = pad_sequences(sequences_r, 100)
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1],1)
 y_train = y_train.reshape(y_train.shape[0], y_train.shape[1],1)
 
-char_to_indices = dict((c, i) for i, c in enumerate(tokenizer.word_index))
-
-indices_to_char = dict((i, c) for i, c in enumerate(tokenizer.word_index))
-
-print (x_train.shape[0])
-print (x_train.shape[1])
-
 X = np.zeros((x_train.shape[0]+1, x_train.shape[1]+1, len(tokenizer.word_index)+1), dtype = np.bool) 
-y = np.zeros((y_train.shape[0]+1, len(tokenizer.word_index)+1), dtype = np.bool) 
+Y = np.zeros((y_train.shape[0]+1, len(tokenizer.word_index)+1), dtype = np.bool) 
 
 for i, sonnet in enumerate(sequences): 
     for t, word in enumerate(sonnet):
         X[i, t, word] = 1        
-    y[i, sequences_r[i]] = 1
+    Y[i, sequences_r[i]] = 1
 
 model = Sequential()
 
-model.add(LSTM(260, input_shape =(101, len(tokenizer.word_index)+1)))
+model.add(LSTM(101, input_shape =(101, len(tokenizer.word_index)+1)))
 model.add(Dense(len(tokenizer.word_index)+1))
-
 model.add(Activation('softmax'))
 
-model.compile(optimizer='adam', 
+model.compile(optimizer='SGD', 
               loss='binary_crossentropy', 
               metrics=['accuracy'])
 
@@ -93,11 +85,14 @@ checkpoint_callback = ModelCheckpoint(model_save_path,
                                       monitor='val_accuracy',
                                       save_best_only=True,
                                       verbose=1)
+filepath = "weights.hdf5"
+
+checkpoint = ModelCheckpoint(filepath, monitor ='loss', verbose = 1, save_best_only = True, mode ='min') 
 
 history = model.fit(X, 
-                    y, 
-                    epochs=5,
-                    batch_size=128,
+                    Y, 
+                    epochs=1,
+                    batch_size=221,
                     validation_split=0.1,
                     callbacks=[checkpoint_callback])
 
