51c8a81aa154d34198bb7db9b2fcd07dc5d2eae2
Apostol6666 <44125687+Apostol6666@users.noreply.github.com>
2020-06-19 21:50:22 +0800

    Update useModel.py

--- useModel.py
+++ useModel.py
@@ -11,6 +11,7 @@ model = load_model('my_model.h5')
 num_words = 10000
 max_length = 101
 
+#Вытаскиваем сонеты в массив sonnets
 def get_sonnets(file):
     sonnet = '';
     sonnets = []
@@ -50,11 +51,12 @@ tokenizer.fit_on_texts(sonnets)
 tokenizer.word_index
 vocabulary = sorted(list(set(tokenizer.word_index))) 
 
+#Создаем два словаря - один переводит цифры в слова, другой - слова в цифры
 num_word = dict((i, c) for i, c in enumerate(vocabulary))
 word_num = dict((c, i) for i, c in enumerate(vocabulary))
 
+#Функция генерации номера
 def sample_index(preds, t = 1.0):
-
     preds = np.asarray(preds).astype('float64')
     preds = np.log(preds) / t
     exp_preds = np.exp(preds)
@@ -62,6 +64,10 @@ def sample_index(preds, t = 1.0):
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas) 
 
+#Функция генерации сонета, length - сколько слов нужно предсказать, до 101 слова
+#Так как это максимальная длина сонета
+#То есть, функцию можно использовать и чтобы просто генерировать случайные сонет со значением 101,
+#И чтобы сгенерировать продолжение какого-либо сонета из нашего файла
 def gen_sonnet(length, diversity, end_index):
 
     start_index = random.randint(0, len(vocabulary) - max_length - 1)
@@ -74,6 +80,10 @@ def gen_sonnet(length, diversity, end_index):
 
     for i in range(length):
             x_pred = np.zeros((1, max_length, len(vocabulary)+1))
+            #В массиве sentence у нас находятся элементы, которые уже взяли из words
+            #То есть просто случайные слова для начала
+            #И наши предсказанные слова
+            
             for t, word in enumerate(sentence):
                 word = word.strip(string.punctuation) 
                 word = word.lower()
@@ -85,10 +95,12 @@ def gen_sonnet(length, diversity, end_index):
             sentence.append(next_word)
     return sentence
 
+#words_get - кол-во слов, кот нужно сгенерировать
 words_get = 101
 words_put = 101 - words_get
 result = gen_sonnet(words_get, 0.2, words_put)
 
+#Функция вывода результата в файл
 def get_result(put):
 
     start = result[0:put]
